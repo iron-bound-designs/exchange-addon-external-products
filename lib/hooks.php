05e3c83461ca9b47fca943e979ade33bb3c0ed63
Timothy Jacobs <timothy@ironbounddesigns.com>
2014-09-08 22:05:29 -0400

    Add list of external products purchased by a particular customer.

--- lib/hooks.php
+++ lib/hooks.php
@@ -142,3 +142,76 @@ function ite_epa_disable_multi_item_cart_on_external_product( $allowed ) {
 }
 
 add_filter( 'it_exchange_multi_item_cart_allowed', 'ite_epa_disable_multi_item_cart_on_external_product', 15 );
+
+/**
+ * Display a users external product purchases.
+ *
+ * @return string
+ */
+function ite_epa_display_users_external_product_purchases() {
+	if ( empty( $_REQUEST['user_id'] ) ) {
+		$user_id = get_current_user_id();
+	} else {
+		$user_id = $_REQUEST['user_id'];
+	}
+
+	$headings = array(
+		__( 'Products', ITE_EPA::SLUG ),
+		__( 'Date', ITE_EPA::SLUG ),
+	);
+
+	$list = array();
+
+	foreach ( ite_epa_get_customer_purchases( $user_id ) as $purchase ) {
+		if ( empty( $purchase ) || empty( $purchase['product'] ) || empty( $purchase['time'] ) ) {
+			continue;
+		}
+
+		$product = $purchase['product'];
+		$time    = $purchase['time'];
+
+		$product = it_exchange_get_product( $product )->post_title;
+		$time    = new DateTime( "@" . $time );
+		$time    = $time->format( get_option( 'date_format' ) );
+
+		$list[] = array( $product, $time );
+	}
+
+	?>
+
+	<h3>External Products Purchased</h3>
+	<div class="user-edit-block products-user-edit-block">
+
+		<div class="heading-row block-row">
+			<?php $column = 0; ?>
+			<?php foreach ( (array) $headings as $heading ) : ?>
+				<?php $column ++ ?>
+				<div class="heading-column block-column block-column-<?php echo $column; ?>">
+					<p class="heading"><?php echo $heading; ?></p>
+				</div>
+			<?php endforeach; ?>
+		</div>
+		<?php foreach ( (array) $list as $item_details ) : ?>
+			<?php $column = 0; ?>
+			<div class="item-row block-row">
+				<?php foreach ( (array) $item_details as $detail ) : ?>
+					<?php $column ++ ?>
+					<?php if ( is_array( $detail ) ) : ?>
+						<div class="item-column block-column block-column-<?php echo $column; ?>">
+							<?php foreach ( $detail as $action => $label ) : ?>
+								<a class="button" href="<?php esc_attr_e( $action ); ?>"><?php esc_attr_e( $label ); ?></a>
+							<?php endforeach; ?>
+						</div>
+					<?php else : ?>
+						<div class="item-column block-column block-column-<?php echo $column; ?>">
+							<p class="item"><?php echo $detail; ?></p>
+						</div>
+					<?php endif; ?>
+				<?php endforeach; ?>
+			</div>
+		<?php endforeach; ?>
+	</div>
+<?php
+}
+
+add_action( 'it-exchange-after-admin-user-products', 'ite_epa_display_users_external_product_purchases' );
